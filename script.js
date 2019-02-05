bb98ca27967453d4512a5b45916b002a201b4dd7
Hugo <Hugo@Hugos-MacBook-Pro.local>
2019-02-05 17:17:21 +0100

    Ajout du bouton de scroll pour la page d'accueil

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
+  // Submenu
   $('#submenu li.active').append('<ul id="anchors"></ul>');
 
   $('h3').each(function() {
     $('#anchors').append('<li>' + $(this).text() + '</li>');
   });
+
+  // Scrolldown
+  $('#home header').append('<span id="scroll-down">découvrir</span>');
+
 });
