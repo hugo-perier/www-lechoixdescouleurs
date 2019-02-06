31d55a9234288342d4ad697119a241fe4f6198e9
Hugo <Hugo@Hugos-MacBook-Pro.local>
2019-02-06 09:47:17 +0100

    Style du menu (desktop)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('#submenu li.active').append('<ul id="anchors"></ul>');
 
   $('h3').each(function() {
-    $('#anchors').append('<li>' + $(this).text() + '</li>');
+    $('#anchors').append('<li><a href=#>' + $(this).text() + '</a></li>');
   });
 
   // Scrolldown
